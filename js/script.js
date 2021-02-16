147aa8c7fc77c697229f96f953c5a457dc624de0
Cristiano <cristiano.tarconi@gmail.com>
2021-02-16 17:08:11 +0100

    Menu Categories section

--- js/script.js
+++ js/script.js
@@ -37,6 +37,58 @@ let app = new Vue({
                 img: 'img/donut-burger-400x571.jpg',
                 price: '6.99'
             }
+        ],
+        menuCat: [
+            {
+                name: 'Appetizers',
+                img: 'img/appetizers-menu-background.jpg'
+            },
+            {
+                name: 'Burgers',
+                img: 'img/burgers-menu-background.jpg'
+            },
+            {
+                name: 'Pizzas',
+                img: 'img/pizza-menu-background.jpg'
+            },
+            {
+                name: 'Fries',
+                img: 'img/fries-menu-background.jpg'
+            },
+            {
+                name: 'Sides',
+                img: 'img/sides-menu-background.jpg'
+            },
+            {
+                name: 'Desserts',
+                img: 'img/desserts-menu-background.jpg'
+            },
+            {
+                name: 'Beverages',
+                img: 'img/beverages-menu-background.jpg'
+            },
+            {
+                name: 'Specials',
+                img: 'img/specials-menu-background.jpg'
+            },
+        ],
+        allergens: [
+            {
+                title: 'Vegetarian',
+                icon: 'img/leaf.svg'
+            },
+            {
+                title: 'Gluten Free',
+                icon: 'img/wheat.svg'
+            },
+            {
+                title: 'Dairy Free',
+                icon: 'img/milk.svg'
+            },
+            {
+                title: 'Keto Friendly',
+                icon: 'img/beef.svg'
+            },
         ]
     },
     mounted() {
